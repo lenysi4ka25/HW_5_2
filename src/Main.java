915ddf3a73af7204146bc699caec2ca8d0ba370b
lenysi4ka <lenysi4ka@mail.ru>
2022-12-25 19:52:39 +0300

    Домашнее задание № 1.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         boolean success = Data.validate("test/", "test8", "test");
         if (success) {
+            System.out.println("Домашка 1");
             System.out.println("Данные корректные!");
             } else {
             System.out.println("Данные не корректные!");
